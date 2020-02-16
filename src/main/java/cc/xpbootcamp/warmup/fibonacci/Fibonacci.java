21f24583cb386854fcccb8bb569024fc562e259d
lishuning <1955142623@qq.com>
2020-02-16 15:13:43 +0800

    return 2 when calculate give 2

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,6 +2,15 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
     public static long calculate(int position) {
-        return 1;
+        long value = 0;
+
+        if (position < 3) {
+            value = 1;
+        }
+        if (position >= 3){
+            value = calculate(position - 1) + calculate(position - 2);
+        }
+
+        return value;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -25,4 +25,14 @@ public class FibonacciTest {
         assertEquals(expectedValue, actualValue);
     }
 
+    @Test
+    void should_return_2_when_calculate_given_position_3() {
+        int position = 3;
+        long expectedValue = 2;
+
+        long actualValue = Fibonacci.calculate(position);
+
+        assertEquals(expectedValue, actualValue);
+
+    }
 }
